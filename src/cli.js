a4fd9ce3e97b6bd1a537abecaa8d1ec7b8837fab
Alex Hernandez <oorangecchicken@gmail.com>
2020-10-16 15:25:45 -0700

    clean up

--- src/cli.js
+++
@@ -1,3 +0,0 @@
-// SERVER CLI
-
-// Coming Soon...
--- src/config.js
+++ src/config.js
@@ -1,23 +1,11 @@
 // SERVER CONFIG
 
-const env = process.env.NODE_ENV || 'development'; // development | production
-const isProd = env === 'production';
-const isDev = env === 'development';
-
 const scheme = 'https';
 const host = 'localhost';
-const domain = 'example.com';
 const port = 443;
 
-const examplePrefix = isProd ? 'www' : 'stage';
-
 module.exports = {
-  env,
-  isProd,
-  isDev,
   scheme,
   host,
-  domain,
   port,
-  examplePrefix,
 };
